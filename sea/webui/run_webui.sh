200a93c08e915a49b5618274013b28e5f9a19a4e
heroding77 <heroding77@qq.com>
2024-06-25 22:48:26 +0800

    Revert "init"
    This reverts commit 29ea1fb5091557df5eefe2a7b9070017dd22bd43.

--- sea/webui/run_webui.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python webui.py \
---model_name_or_path /cpfs01/user/dingzichen/projects/mistral/review_model/SEA-E/ \
---template mistral \
---max_new_tokens 8192 \
---infer_type transformers
