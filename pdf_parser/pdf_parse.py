ffe957b31736b5afde37283e08e12b6d2a7b544f
jianxiangyu <88993491+jianxiangyu@users.noreply.github.com>
2024-06-27 13:49:33 +0800

    Update pdf_parse.py

--- pdf_parser/pdf_parse.py
+++ pdf_parser/pdf_parse.py
@@ -1,7 +1,5 @@
 import subprocess
-from PyPDF2 import PdfReader, PdfWriter
 from .utils import *
-from tqdm import tqdm
 import argparse
 import os
 parser = argparse.ArgumentParser()
@@ -13,11 +11,11 @@ args, _ = parser.parse_known_args()
 
 def parse(file, output_dir, ifrm):
     '''
-        file: parse PDF file to be parsed
+        file: PDF file to be parsed
         output_dir: the output directory
-        ifrm: if or not to delete the pdf file
+        ifrm: if or not to delete the PDF file
     '''
-    # nougat ../temp/out_part_1.pdf -o ./ -m 0.1.0-base
+    # nougat ../test.pdf -o ./ -m 0.1.0-base
     result = subprocess.run(["conda", "deactivate"], capture_output=True, text=True)
     command_parse = ["nougat", file, "-o", output_dir, "-m", "0.1.0-base", "--no-skipping","--batchsize","8"]
     # run 
@@ -32,9 +30,6 @@ def parse(file, output_dir, ifrm):
         else:
             print("err:"+result.stderr)
             print("Command failed with exit code:", result.returncode)
-            with open('error.txt', 'a') as f1:
-                f1.write(file + '\n')
-            f1.close()
     except FileNotFoundError as e:
         print(f"The command 'nougat' was not found: {e}")
 
@@ -46,7 +41,6 @@ def run_parse(pdf_path):
     save_path = os.path.join(mmd_dir,file_name + '.mmd')
     parse(pdf_path, mmd_dir , ifrm=False)
     return save_path
-
 
 if __name__=='__main__':
     run_parse("./test.pdf")
